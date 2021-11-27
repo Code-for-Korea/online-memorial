66bf9d3e0110fe5e6743a106cada450caf150c10
Taeyang Jin <sun@jupiterflow.com>
2021-11-27 23:35:39 +0900

    add HelloRestController

--- server-hall/src/main/kotlin/kr/codefor/memorial/server/hall/ServerHallApplication.kt
+++ server-hall/src/main/kotlin/kr/codefor/memorial/server/hall/ServerHallApplication.kt
@@ -2,10 +2,19 @@ package kr.codefor.memorial.server.hall
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ServerHallApplication
 
 fun main(args: Array<String>) {
-	runApplication<ServerHallApplication>(*args)
+    runApplication<ServerHallApplication>(*args)
+}
+
+@RestController
+class HelloRestController {
+
+    @GetMapping("/hello")
+    fun hello() = "Hello, memorial-server-hall"
 }
