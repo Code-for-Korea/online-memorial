9df6e5c511a55b1fb61b0415bc454ac3fa3af4dc
Taeyang Jin <sun@jupiterflow.com>
2021-11-27 16:49:52 +0900

    추모관 서버 셋업

---
+++ server-hall/src/main/kotlin/kr/codefor/memorial/server/hall/ServerHallApplication.kt
@@ -0,0 +1,11 @@
+package kr.codefor.memorial.server.hall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerHallApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerHallApplication>(*args)
+}
---
+++ server-hall/src/test/kotlin/kr/codefor/memorial/server/hall/ServerHallApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.codefor.memorial.server.hall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerHallApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
