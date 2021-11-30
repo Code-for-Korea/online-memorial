8bfa857444064d21a357942e5e5848317603419c
Taeyang Jin <sun@jupiterflow.com>
2021-12-01 00:10:57 +0900

    EncryptHolder 복호화 테스트 추가

---
+++ server-hall/src/test/kotlin/kr/codefor/memorial/server/hall/EncryptHolderTest.kt
@@ -0,0 +1,21 @@
+package kr.codefor.memorial.server.hall
+
+import kr.codefor.memorial.server.hall.configuration.environment.AesEncryptHolder
+import org.junit.jupiter.api.Test
+
+class EncryptHolderTest {
+
+    private val encrypt = AesEncryptHolder(
+        key = "A1B2C3D4E5F6G7H8",
+        iv = "8H7G6F5E4D3C2B1A",
+        cipherTransformation = "AES/CBC/PKCS5Padding"
+    )
+
+    @Test
+    fun encrypt() {
+        val enc = encrypt.enc("user-12345")
+        val dec = encrypt.dec(enc)
+
+        assert(dec == "user-12345")
+    }
+}
