e38b49b0472c233a17bba6a14e8ef9e6f126ec20
Taeyang Jin <sun@jupiterflow.com>
2021-12-01 11:00:57 +0900

    Configuration/Dependencies: Cors, QueryDsl 설정

--- server-hall/build.gradle.kts
+++ server-hall/build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
     kotlin("plugin.jpa") version "1.6.0"
+    kotlin("kapt") version "1.6.0"
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 group = "kr.codefor.memorial"
@@ -17,13 +24,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // JPA & MariaDB Driver
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    // Querydsl
+    implementation("com.querydsl:querydsl-jpa")
+    kapt(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("io.strikt:strikt-core:0.32.0")
 }
 
 tasks.withType<KotlinCompile> {
