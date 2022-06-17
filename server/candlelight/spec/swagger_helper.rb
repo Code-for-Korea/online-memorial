ddae51569e238dbb716b86515f3bf9494fd1cd99
KWEON, Ohyeon <rest515@gmail.com>
2022-06-17 22:01:06 +0900

    feat: swagger 호스트명을 수정합니다

--- server/candlelight/spec/swagger_helper.rb
+++ server/candlelight/spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://{defaultHost}',
+          url: 'https://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
+              default: 'candle-c4k.herokuapp.com'
             }
           }
         }
