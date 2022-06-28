4554d3e152e85acd5f175f4e42bdb7b23361bbc2
swimmingkiim <swimmingkiim@gmail.com>
2022-06-28 12:21:30 +0900

    trigger build

---
+++ client/node_modules/@storybook/cli/dist/ts3.4/generators/ANGULAR/angular-helpers.d.ts
@@ -0,0 +1,5 @@
+export declare function getAngularAppTsConfigPath(): any;
+export declare function getAngularAppTsConfigJson(): any;
+export declare function editStorybookTsConfig(tsconfigPath: string): void;
+export declare function isDefaultProjectSet(): boolean;
+export declare function getBaseTsConfigName(): Promise<"tsconfig.base.json" | "tsconfig.json">;
